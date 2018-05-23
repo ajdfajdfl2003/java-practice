69be42340b538f6cf563671ee90edae5dd6bd0e6
AngusWu <anguswu@simbo.com.tw>
2018-05-23 16:42:41 +0800

    Add LoadClass

---
+++ src/main/java/LoadClass.java
@@ -0,0 +1,13 @@
+import practice.TestLoadClass;
+
+public class LoadClass {
+    public static void main(String[] args) {
+        TestLoadClass loader = null;
+
+        System.out.println("Declare reference name of TestLoadClass");
+
+        loader = new TestLoadClass();
+
+        System.out.println("Create a instance of TestLoadClass ");
+    }
+}
---
+++ src/main/java/practice/TestLoadClass.java
@@ -0,0 +1,7 @@
+package practice;
+
+public class TestLoadClass {
+    static {
+        System.out.println("This class has been loaded.");
+    }
+}
