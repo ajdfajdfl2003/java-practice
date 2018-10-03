6fe9809ce5d669bceaedd3ddfb7bdbdf2f229ed3
AngusWu <anguswu@simbo.com.tw>
2018-10-04 00:02:16 +0800

    exchange positions of static field and static variable of inner class

--- src/main/java/practice/Singleton.java
+++ src/main/java/practice/Singleton.java
@@ -3,11 +3,11 @@ package practice;
 public class Singleton {
 
     static class Inner {
+        static final Singleton testInstance = new Singleton(3);
+
         static {
             System.out.println("Inner Statuc Field");
         }
-
-        static final Singleton testInstance = new Singleton(3);
     }
 
     public static Singleton getInstance() {
