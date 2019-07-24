f1e2e852f7e6a2ad0da0662f32aebb630e4b0c0b
AngusWu <ajdfajdfl2003@gmail.com>
2019-07-24 10:14:25 +0800

    use Optional ifPresent

--- src/main/java/demo/OptionalDemo.java
+++ src/main/java/demo/OptionalDemo.java
@@ -9,26 +9,33 @@ import java.util.Optional;
  * 因為忘記 Optional ，所以透過 https://segmentfault.com/a/1190000008692522 再練習一次
  */
 public class OptionalDemo {
+    private static UserService service;
+
     public static void main(String[] args) {
+        service = new UserService();
         UseIfCondition();
         UseIsPresent();
+        UserIfPresent();
+    }
+
+    private static void UserIfPresent() {
+        Optional<User> user = service.getUserById(2018);
+        user.ifPresent(u -> System.out.println(String.format("Username is: %s", u.getUserName())));
     }
 
     private static void UseIsPresent() {
-        UserService service = new UserService();
         Optional<User> user = service.getUserById(2018);
         if (user.isPresent()) {
             String username = user.get().getUserName();
-            System.out.println("Username is: " + username);
+            System.out.println(String.format("Username is: %s", username));
         }
     }
 
     private static void UseIfCondition() {
-        UserService service = new UserService();
         User user = service.getUserByName("Angus");
         if (user != null) {
             String username = user.getUserName();
-            System.out.println("Username is: " + username);
+            System.out.println(String.format("Username is: %s", username));
         }
     }
 }
