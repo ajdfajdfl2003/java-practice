d994e6a8ffc5ece3bf9a0aa7a84123e9635f8a9c
anguswu <ajdfajdfl2003@gmail.com>
2021-05-23 20:31:26 +0800

    feat: run vs start

--- src/main/java/thread/ANewThread.java
+++ src/main/java/thread/ANewThread.java
@@ -16,11 +16,13 @@ public class ANewThread {
         final Thread thread = new Thread(runnable);
         thread.setName("test_thread_1");
         thread.setDaemon(true);
-        thread.start();
+        thread.start(); // 使用 新線程 去執行
+//        thread.run(); // 使用主 thread 去執行
 
         //second way:
-        new ExtendThread("test_thread_2", true).start();
+        new ExtendThread("test_thread_2", true).start(); // 使用 新線程 去執行
+//        new ExtendThread("test_thread_2", true).run(); // 使用主 thread 去執行
 
-        TimeUnit.SECONDS.sleep(6);
+//        TimeUnit.SECONDS.sleep(6);
     }
 }
