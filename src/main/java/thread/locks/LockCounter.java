8f4042d5c5645fcbed9f8a566c49bf556361b699
anguswu <ajdfajdfl2003@gmail.com>
2021-05-27 00:01:30 +0800

    feat: practice Lock

---
+++ src/main/java/thread/locks/LockCounter.java
@@ -0,0 +1,22 @@
+package thread.locks;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockCounter {
+    private final Lock lock = new ReentrantLock(true);
+    private int sum = 0;
+
+    public int addAndGet() {
+        try {
+            lock.lock();
+            return ++sum;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getSum() {
+        return sum;
+    }
+}
