d55a90423f6e8359616460c7eed28b8d481d2e65
anguswu <anguswu@simbo.com.tw>
2023-02-16 11:17:27 +0800

    refactor: use mapToInt

--- src/main/java/demo/MostWordsFound.java
+++ src/main/java/demo/MostWordsFound.java
@@ -1,13 +1,12 @@
 package demo;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class MostWordsFound {
     public int mostWordsFound(String[] sentences) {
         return Arrays.stream(sentences)
                 .map(String::trim)
-                .map(s -> (int) Arrays.stream(s.split("\\s+")).count())
-                .max(Integer::compareTo).orElse(0);
+                .mapToInt(s -> (int) Arrays.stream(s.split("\\s+")).count())
+                .max().orElse(0);
     }
 }
